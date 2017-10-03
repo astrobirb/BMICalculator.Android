8cdcfb04bcafd4d22011aba5cec7e354897380cf
astrobirb <31900770+astrobirb@users.noreply.github.com>
2017-10-03 13:51:41 -0600

    end of class

--- MainActivity.cs
+++ MainActivity.cs
@@ -7,7 +7,9 @@ namespace BMICalculator.Android
     [Activity(Label = "BMICalculator.Android", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        Button calculateButton;
+        TextView resultTextView;
+        EditText heightEditText, weightEditText;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -15,6 +17,23 @@ namespace BMICalculator.Android
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            calculateButton = FindViewById<Button> (BMICalculator.Android.Resources.Id.calculateButton);
+            resultTextView = FindViewById<TextView> (BMICalculator.Android.Resources.Id.resultTextView);
+            heightEditText = FindViewById<EditText> (BMICalculator.Android.Resources.Id.heightEdittext);
+            weightEditText = FindViewById<EditText> (BMICalculator.Android.Resources.Id.weightEditText);
+
+            calculateButton.Click += CalculateButton_Click;
+        }
+
+        void CalculateButton_Click (object sender, System.EventArgs e)
+        {
+            float height = float.Parse (heightEditText.Text);
+            float weight = float.Parse (weightEditText.Text);
+
+            float bmi = (weight / (height * height));
+
+            resultTextView.Text = bmi.ToString();
         }
     }
 }
